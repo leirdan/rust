a2ed97ebf152b33e4acb33d6ab64347c2a8d5230
leirdan <uouleirdan@gmail.com>
2025-09-06 09:51:05 -0300

    guess game improved

--- guess-game/src/main.rs
+++ guess-game/src/main.rs
@@ -3,19 +3,20 @@ use std::io;
 use std::str::FromStr;
 
 fn main() -> io::Result<()> {
-    let rng = rand::rng();
-    let res: i32 = rng.random_range(1..=100);
+    let res: i32 = rand::rng().random_range(1..=100);
 
     loop {
         let mut guess = String::new();
         io::stdin().read_line(&mut guess)?;
 
         match i32::from_str(&guess.trim()) {
-            Err(e : Err) => {
+            Err(_) => {
                 println!("Insert a valid number!");
             }
-            Ok(n: i32) => {
-                if n == res {
+            Ok(n) => {
+                if n > 100 || n < 0 {
+                    println!("Guess outside the valid range..");
+                } else if n == res {
                     println!("You won!");
                     break;
                 } else if n < res {
