6583b36e4cd2fe11a9545d5816ceac70ca63e1e8
leirdan <uouleirdan@gmail.com>
2025-11-05 14:39:11 -0300

    [mini-parser] tree increments

--- mini-parser/src/main.rs
+++ mini-parser/src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::VecDeque;
-use std::num::ParseIntError;
 
 fn next(input: &str) -> Result<(usize, &str, &str), Option<usize>> {
     let mut start_idx = 0;
@@ -53,6 +52,7 @@ fn retrieve_tokens(mut input: &str) -> Box<Vec<&str>> {
     tokens
 }
 
+#[derive(Clone)]
 enum Expression<'a> {
     Number(i64),
     Symbol(char),
@@ -60,59 +60,135 @@ enum Expression<'a> {
     Exp {
         left: Box<Expression<'a>>,
         right: Box<Expression<'a>>,
-        operation: char,
+        operation: Box<Expression<'a>>,
     },
 }
 
 impl Expression<'_> {
     fn evaluate(&self) -> Option<i64> {
-        None
-    }
-
-    fn print(&self) {}
-
-    fn tree(&self) {
         match self {
+            Expression::Number(num) => Some(*num),
+            Expression::Symbol(sym) => None,
+            Expression::String(_) => None,
             Expression::Exp {
                 left,
                 right,
                 operation,
             } => {
-                println!("{}", operation);
-                left.tree_rec("", false);
-                right.tree_rec("", true);
+                let l = left.evaluate();
+                let r = right.evaluate();
+
+                if let Some(v_l) = l
+                    && let Some(v_r) = r
+                {
+                    if let Expression::Symbol(sym) = **operation {
+                        match sym {
+                            '+' => Some(v_l + v_r),
+                            '-' => Some(v_l - v_r),
+                            '*' => Some(v_l * v_r),
+                            '/' => {
+                                if v_r == 0 {
+                                    println!("Div by 0 is not allowed");
+                                    None
+                                } else {
+                                    Some(v_l / v_r)
+                                }
+                            }
+                            '%' => Some(v_l % v_r),
+                            _ => None,
+                        }
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
             }
-            _ => self.tree_rec("", true),
         }
     }
 
-    fn tree_rec(&self, tab: &str, last: bool) {
-        let prefix: &str = if last { "└" } else { "├" };
+    fn print(&self) {}
+
+    fn convert_tokens_to_exp() {}
+
+    fn tree(&self) {
+        self.tree_rec("", true, true);
+    }
+
+    fn tree_rec<'a>(&self, tab: &str, last: bool, root: bool) {
+        let create_exp = |v: &mut VecDeque<Expression<'a>>| {
+            let mut idx: Option<usize> = None;
+            for (i, ex) in v.iter().enumerate() {
+                if let Expression::Symbol(sym) = ex {
+                    match sym {
+                        '*' | '%' | '/' => {
+                            idx = Some(i);
+                            break;
+                        }
+                        '+' | '-' => {
+                            idx = Some(i);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+
+            let Some(mut i) = idx else { todo!() };
+
+            let l = Box::new(v.remove(i - 1).unwrap());
+            let op = Box::new(v.remove(i - 1).unwrap());
+            i = if i > v.len() { v.len() } else { i };
+            let r = Box::new(v.remove(i - 1).unwrap());
+
+            Expression::Exp {
+                left: l,
+                right: r,
+                operation: op,
+            }
+        };
 
         match self {
             Expression::Number(num) => {
-                println!("{}{} {}", tab, prefix, num);
+                println!(
+                    "{}{}{}",
+                    tab,
+                    if root {
+                        ""
+                    } else if last {
+                        "└─ "
+                    } else {
+                        "├─ "
+                    },
+                    num
+                );
             }
             Expression::Symbol(sym) => {
-                println!("{}{} {}", tab, prefix, sym);
+                println!(
+                    "{}{}{}",
+                    tab,
+                    if root {
+                        ""
+                    } else if last {
+                        "└─ "
+                    } else {
+                        "├─ "
+                    },
+                    sym
+                );
             }
             Expression::String(tokens) => {
                 let mut symbols: VecDeque<Expression> = VecDeque::new();
-                let mut operations: Vec<char> = Vec::new();
+                let mut expressions: Vec<Expression> = Vec::new();
 
                 for &token in tokens {
                     match token {
-                        "+" | "-" | "%" | "/" | "*" => operations.push(token.parse().unwrap()),
-                        "(" => {}
+                        "+" | "-" | "%" | "/" | "*" => {
+                            symbols.push_back(Expression::Symbol(token.parse().unwrap()))
+                        }
                         ")" => {
-                            let l = Box::new(symbols.pop_front().unwrap());
-                            let r = Box::new(symbols.pop_front().unwrap());
-
-                            symbols.push_back(Expression::Exp {
-                                left: l,
-                                right: r,
-                                operation: operations.pop().unwrap(),
-                            })
+                            let exp = create_exp(&mut symbols);
+                            symbols.push_front(exp.clone());
+                            expressions.push(exp);
                         }
                         num => match num.parse::<i64>() {
                             Ok(n) => {
@@ -124,17 +200,12 @@ impl Expression<'_> {
                 }
 
                 while symbols.len() != 1 {
-                    let l = Box::new(symbols.pop_front().unwrap());
-                    let r = Box::new(symbols.pop_front().unwrap());
-
-                    symbols.push_back(Expression::Exp {
-                        left: l,
-                        right: r,
-                        operation: operations.pop().unwrap(),
-                    })
+                    let exp = create_exp(&mut symbols);
+                    symbols.push_front(exp.clone());
+                    expressions.push(exp);
                 }
 
-                if let Some(exp) = symbols.pop_front() {
+                if let Some(exp) = expressions.pop() {
                     exp.tree();
                 }
             }
@@ -143,18 +214,17 @@ impl Expression<'_> {
                 right,
                 operation,
             } => {
-                println!("{}{} {}", tab, prefix, operation);
-                let pre = format!("{}{}", tab, if last { " " } else { "| " });
-                left.tree_rec(&pre, false);
-                right.tree_rec(&pre, true);
+                operation.tree_rec(tab, false, root);
+                let pre: &str = &format!("{}{}", tab, if last { "   " } else { "│  " });
+                left.tree_rec(&pre, false, false);
+                right.tree_rec(&pre, true, false);
             }
         }
     }
 }
 
 fn main() {
-    let mut tokens: Vec<&str> = Vec::new();
-    let str = "(10 + 20) * 30";
+    let str = "10 + 20 * 30";
     let tokens = retrieve_tokens(str);
 
     let expr = Expression::String(*tokens);
@@ -167,9 +237,11 @@ mod tests {
 
     #[test]
     fn exp1() {
-        let str = "10 + 20 * 30"; // TODO: verificar esse erro
-        let tokens = retrieve_tokens(str);
-        Expression::String(*tokens).tree();
-
+        // let str = "(-(10 + 20) + 30 + 40 + (50 + 60)) * -5"; // TODO: TÁ COM PROBLEMA AINDA
+        Expression::String(*retrieve_tokens("10 + 20")).tree();
+        Expression::String(*retrieve_tokens("10 / 0")).tree();
+        Expression::String(*retrieve_tokens("10 + 20")).tree();
+        Expression::String(*retrieve_tokens("(10 + 20) * 30")).evaluate();
+        Expression::String(*retrieve_tokens("10 + 20 * 30")).tree();
     }
 }
